e1c7056b316040cbb3d7851ddf2f623a04fae9a3
Mike Lucent <mike.j.lucent@gmail.com>
2021-12-27 18:18:57 -0800

    Created PDF report and cleaned up code

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from fpdf import FPDF
+import webbrowser
+import os
+
 class Bill:
     """
     Object that contains data about a bill, such as total 
@@ -19,8 +23,8 @@ class Flatmate:
         self.name = name
         self.days_in_house= days_in_house
 
-    def pays(self, bill, flatemate2):
-        weight = self.days_in_house / (self.days_in_house + flatemate2.days_in_house)
+    def pays(self, bill, flatmate2):
+        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
         return bill.amount * weight 
 
 
@@ -36,12 +40,49 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+
+        flatmate1_pays = str(round(flatmate1.pays(bill=bill, flatmate2=flatmate2), 2))
+
+        flatmate2_pays = str(round(flatmate2.pays(bill=bill, flatmate2=flatmate1), 2))
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        #Add house icon to top
+        pdf.image(name = "house.png", w=30, h=30)
+
+        #Insert Title 
+        pdf.set_font(family='Times', size = 24, style='B')
+        pdf.cell(w=0, h=80, txt='Flatemates Bill', border=0, align='C', ln=1)
+
+        #Insert Period label and value
+        pdf.set_font(family= "Times", size=14, style ='B')
+        pdf.cell(w=100, h=40, txt='Period:', border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        #Insert Name and due Amount of first Flatmate
+        pdf.set_font(family="Times", size=12)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate1_pays, border=0, ln=1)
+
+        #Insert Name and due Amount of first Flatmate
+        pdf.set_font(family="Times", size=12)
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate2_pays, border=0, ln=1)
+
+        #Print the Pdf
+        pdf.output(self.filename)
+
+        #Open PDF automaticlly, if windows : webbrowser.open(self.filename)
+        webbrowser.open('file://'+os.path.realpath(self.filename))
 
 
 the_bill = Bill(amount=120, period="March 2021")
 john = Flatmate(name="John", days_in_house=20)
 mary = Flatmate(name="Mary", days_in_house=25)
 
-print("John Pays: ", john.pays(bill=the_bill, flatemate2=mary))
-print("Mary Pays: ", mary.pays(bill=the_bill, flatemate2=john))
+print("John Pays: ", john.pays(bill=the_bill, flatmate2=mary))
+print("Mary Pays: ", mary.pays(bill=the_bill, flatmate2=john))
+
+pdf_report = PdfReport(filename  = "Report1.pdf")
+pdf_report.generate(flatmate1=john, flatmate2= mary, bill=the_bill)
